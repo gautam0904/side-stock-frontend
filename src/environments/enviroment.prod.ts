437a1fbb26f258519c75591ff21fd3d71305fbad
Gautam Malaviya <malaviyagautam0942@gmail.com>
2025-02-01 19:01:50 +0530

    env

--- src/environments/enviroment.prod.ts
+++ src/environments/enviroment.prod.ts
@@ -1,5 +1,3 @@
-console.log(import.meta.env.VITE_BACKEND);
-console.log(import.meta.env.VITE_BACKEND_URL);
 export const environment = {
-    apiUrl: 'http://localhost:3031/api/v1'
+    apiUrl: import.meta.env.VITE_BACKEND
 }
